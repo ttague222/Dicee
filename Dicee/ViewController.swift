a3334c33f073ffbdad759a4ec6d6a5486b60cdd3
Thomas Tague <ttague222@gmail.com>
2018-10-25 19:34:28 -0500

    Initial Commit

---
+++ Dicee/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by Thomas Tague on 10/25/18.
+//  Copyright © 2018 Thomas Tague. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
